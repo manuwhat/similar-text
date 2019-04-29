8b679deb49a40850c48e24eaeffba839ada8491a
manuwhat <leizmo@gmail.com>
2019-04-29 18:59:45 +0200

    cs fixed

--- src/similar_text.php
+++ src/similar_text.php
@@ -146,7 +146,7 @@ namespace EZAMA{
             if (is_array($split)) {
                 return
                     array_map(
-                        function($val) {
+                        function ($val) {
                             if (self::is_ascii($val)) {
                                 return strtolower($val);
                             }
